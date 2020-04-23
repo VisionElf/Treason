3da377805043fe0eecda8ad1a88fe1f3ddf36703
Kevin Waser <kevin@voodoo.io>
2020-04-23 19:06:12 +0200

    Edited HoveringObject

--- Assets/HoveringObject.cs
+++ Assets/HoveringObject.cs
@@ -2,40 +2,27 @@
 
 public class HoveringObject : MonoBehaviour
 {
-    public float maxOffset;
-    public float speed;
+    public Vector3 targetOffset;
+    public float duration;
     public AnimationCurve easing;
 
     private Vector3 _startPos;
-    private Vector3 _direction;
     private float _currentTime;
+    private float _mult;
 
     private void Start()
     {
         _startPos = transform.position;
-        _direction = Vector3.up;
         _currentTime = 0f;
+        _mult = 1f;
     }
 
     void Update()
     {
-        if (_direction == Vector3.up)
-        {
-            if (_currentTime < 1.0f)
-                transform.position = _startPos + new Vector3(0f, easing.Evaluate(_currentTime) * maxOffset);
-            else
-                _direction = Vector3.down;
-
-            _currentTime = Mathf.Clamp(_currentTime + speed, 0f, 1.0f);
-        }
-        else if (_direction == Vector3.down)
-        {
-            if (_currentTime > 0f)
-                transform.position = _startPos + new Vector3(0f, easing.Evaluate(_currentTime) * maxOffset);
-            else
-                _direction = Vector3.up;
-
-            _currentTime = Mathf.Clamp(_currentTime - speed, 0f, 1.0f);
-        }
+        _currentTime += Time.deltaTime * _mult;
+        var percent = Mathf.Clamp01(_currentTime / duration);
+        if (percent >= 1f || percent <= 0f) _mult *= -1;
+        
+        transform.position = Vector3.Lerp(_startPos, _startPos + targetOffset, easing.Evaluate(percent));
     }
 }
