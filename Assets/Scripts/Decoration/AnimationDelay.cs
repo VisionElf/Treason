b4c39f8e8beb43047910dc4e6d13a771fb628ca1
wRadion <wradion@gmail.com>
2020-04-24 18:30:11 +0200

    [Decorations]: Added Weapons red lights

---
+++ Assets/Scripts/Decoration/AnimationDelay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AnimationDelay : MonoBehaviour
+{
+    public float delay;
+    public string booleanName;
+
+    void Start()
+    {
+        StartCoroutine(DelayAnimation());
+    }
+
+    IEnumerator DelayAnimation()
+    {
+        yield return new WaitForSeconds(delay);
+        GetComponent<Animator>().SetBool(booleanName, true);
+    }
+}
