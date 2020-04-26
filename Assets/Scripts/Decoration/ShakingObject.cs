c319e59cdfacc8d820db5a14fd6b756ff4c5f30c
wRadion <wradion@gmail.com>
2020-04-26 22:33:55 +0200

    [Decorations]: Finished all decorations! (security + ship thrusters)

---
+++ Assets/Scripts/Decoration/ShakingObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ShakingObject : MonoBehaviour
+{
+    public float intensity;
+    public float period;
+
+    private float _time;
+
+    private void Start()
+    {
+        _time = 0f;
+    }
+
+    void Update()
+    {
+        _time += Time.deltaTime;
+
+        if (_time >= period)
+        {
+            transform.localPosition = transform.localPosition + (Random.insideUnitSphere * intensity);
+            _time = 0;
+        }
+    }
+}
