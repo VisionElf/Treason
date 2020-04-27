52a01677008a58419bce160931b6d3828e0390f1
wRadion <wradion@gmail.com>
2020-04-27 15:25:39 +0200

    [Minimap]: Added minimap prototype

---
+++ Assets/Scripts/Gameplay/Minimap.cs
@@ -0,0 +1,32 @@
+﻿using Gameplay;
+using System.Linq;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform astronautIcon;
+    public Vector3 offset;
+    public float scale;
+    public GameObject graphics;
+
+    private Astronaut _player;
+
+    private void Start()
+    {
+        _player = null;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+            graphics.SetActive(!graphics.activeSelf);
+
+        if (!graphics.activeSelf) return;
+
+        if (_player == null)
+            _player = FindObjectsOfType<Astronaut>().First((p) => p.isLocalCharacter);
+
+        astronautIcon.localPosition = (_player.transform.position / scale) - offset;
+    }
+}
