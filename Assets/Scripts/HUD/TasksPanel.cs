882a93ef10e40a4738e36867e50fa2beb23adc58
Kevin Waser <kevin@voodoo.io>
2020-04-27 17:50:00 +0200

    Added DOTween and basic InGameHUD

---
+++ Assets/Scripts/HUD/TasksPanel.cs
@@ -0,0 +1,49 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TasksPanel : MonoBehaviour
+{
+    public RectTransform taskList;
+    public LayoutElement panelElt;
+    public Button tabButton;
+    public float slideDuration = 0.4f;
+    
+    private RectTransform _rectTransform;
+    private bool _isShowing;
+
+    private void Awake()
+    {
+        _isShowing = true;
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void OnEnable()
+    {
+        tabButton.onClick.AddListener(Toggle);
+    }
+    
+    private void OnDisable()
+    {
+        tabButton.onClick.RemoveListener(Toggle);
+    }
+
+    private void Toggle()
+    {
+        _isShowing = !_isShowing;
+        var mult = _isShowing ? 0f : -1f;
+
+        var pos = _rectTransform.anchoredPosition;
+        pos.x = mult * panelElt.preferredWidth;
+        _rectTransform.DOComplete();
+        _rectTransform.DOAnchorPos(pos, slideDuration);
+    }
+
+    private void Update()
+    {
+        if (taskList && panelElt)
+        {
+            panelElt.preferredWidth = taskList.sizeDelta.x;
+        }
+    }
+}
---
+++ Assets/Scripts/HUD/TasksPanelItem.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace HUD
+{
+    public class TasksPanelItem : MonoBehaviour
+    {
+        public TMP_Text taskText;
+
+        private RectTransform _textRectTransform;
+        private RectTransform _rectTransform;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _textRectTransform = taskText.GetComponent<RectTransform>();
+        }
+
+        public void SetText(string text, Color color)
+        {
+            taskText.text = text;
+            taskText.color = color;
+        }
+
+        private void Update()
+        {
+            var sizeDelta = _rectTransform.sizeDelta;
+            var width = _textRectTransform.sizeDelta.x;
+            sizeDelta.x = width;
+            _rectTransform.sizeDelta = sizeDelta;
+        }
+    }
+}
