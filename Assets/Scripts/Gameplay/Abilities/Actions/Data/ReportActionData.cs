bbe757c0d8666f54f20106d3183c469dae71ce43
wRadion <wradion@gmail.com>
2020-05-02 00:54:24 +0200

    [Report]: Added report action

---
+++ Assets/Scripts/Gameplay/Abilities/Actions/Data/ReportActionData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Abilities.Actions.Data
+{
+    [CreateAssetMenu]
+    public class ReportActionData : ActionData
+    {
+        public override void Execute(ActionContext context)
+        {
+            var target = context.Get<Astronaut>(Context.Target);
+
+            if (target != null)
+            {
+                Debug.Log("Reported!");
+                Destroy(target.gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/Gameplay/DeadAstronaut.cs
+++ Assets/Scripts/Gameplay/DeadAstronaut.cs
@@ -1,38 +1,8 @@
 ﻿using Gameplay;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 namespace Interactables
 {
     public class DeadAstronaut : Interactable
     {
-        private void LateUpdate()
-        {
-            var localCharacter = Astronaut.LocalAstronaut;
-
-            if (localCharacter)
-            {
-                Vector2 pos = transform.position;
-                Vector2 localPos = localCharacter.transform.position;
-
-                var dir = pos - localPos;
-                var dist = dir.magnitude;
-
-                var visible = false;
-                if (dist <= localCharacter.visionRange)
-                    visible = !Physics2D.Raycast(localPos, dir.normalized, dist, localCharacter.visibleLayerMask);
-
-                if (visible && dist <= interactRange)
-                    localCharacter.SetReportInteract(this, dist);
-                else
-                    localCharacter.RemoveReportInteract(this);
-            }
-        }
-
-        public override void Interact()
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
