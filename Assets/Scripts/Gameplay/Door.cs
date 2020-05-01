50a2863787827ddffdc4e81377fb04560705bb43
Kevin Waser <kevin@voodoo.io>
2020-05-01 19:28:01 +0200

    Added doors

---
+++ Assets/Scripts/Gameplay/Door.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class Door : MonoBehaviour
+    {
+        private Animator _animator;
+        private bool _closed;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            Open();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SetClosed(!_closed);
+            }
+        }
+
+        private void SetClosed(bool closed)
+        {
+            _closed = closed;
+            _animator.SetBool("Closed", _closed);
+        }
+
+        public void Open()
+        {
+            SetClosed(false);
+        }
+
+        public void Close()
+        {
+            SetClosed(true);
+        }
+    }
+}
