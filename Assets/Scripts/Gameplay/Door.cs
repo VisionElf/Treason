54b7b5b77cc31e7a0d7ac9e44956b464fb4e8b20
Kevin Waser <kevin@voodoo.io>
2020-05-01 23:16:20 +0200

    Added Door sound

--- Assets/Scripts/Gameplay/Door.cs
+++ Assets/Scripts/Gameplay/Door.cs
@@ -5,13 +5,17 @@ namespace Gameplay
     public class Door : MonoBehaviour
     {
         public float closeDuration;
+        public AudioClip closeSound;
+        public AudioClip openSound;
         
         private Animator _animator;
         private bool _closed;
+        private AudioSource _audioSource;
 
         private void Awake()
         {
             _animator = GetComponent<Animator>();
+            _audioSource = GetComponent<AudioSource>();
         }
 
         private void Start()
@@ -19,16 +23,13 @@ namespace Gameplay
             Open();
         }
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SetClosed(!_closed);
-            }
-        }
-
         private void SetClosed(bool closed)
         {
+            if (_closed != closed)
+            {
+                if (closed) _audioSource.PlayOneShot(closeSound);
+                else _audioSource.PlayOneShot(openSound);
+            }
             _closed = closed;
             _animator.SetBool("Closed", _closed);
         }
