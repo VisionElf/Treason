25dec1140ef8649201e054a95c1548006e8fba8d
Kevin Waser <kevin@voodoo.io>
2020-05-03 16:32:21 +0200

    Added DottedLine Mat

---
+++ Assets/DottedLine.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DottedLine : MonoBehaviour
+{
+    private Material _instantiatedMaterial;
+    
+    private void Start()
+    {
+        var img = GetComponent<Image>();
+        _instantiatedMaterial = Instantiate(img.material);
+        img.material = _instantiatedMaterial;
+        
+        var width = transform.parent.GetComponent<RectTransform>().sizeDelta.x;
+        _instantiatedMaterial.SetFloat("_Width", width);
+    }
+}
