a52044fbb3b19ec1a049a5c65ac6bbd8662410f2
wRadion <wradion@gmail.com>
2020-05-04 13:35:23 +0200

    [Tasks]: Revamp task into separate UseTaskAbility

---
+++ Assets/Scripts/Gameplay/Abilities/Actions/Data/UseTaskActionData.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Gameplay.Interactables;
+
+namespace Gameplay.Abilities.Actions.Data
+{
+    [CreateAssetMenu]
+    public class UseTaskActionData : ActionData
+    {
+        public override void Execute(ActionContext context)
+        {
+            Astronaut astronaut = context.Get<Astronaut>(Context.Source);
+            TaskObject target = context.Get<TaskObject>(Context.Target);
+
+            if (target != null)
+            {
+                if (astronaut.TaskState == AstronautTaskState.IN_TASK && target.IsCancellable)
+                {
+                    astronaut.TaskState = AstronautTaskState.NONE;
+                    target.StopTask();
+                }
+                else
+                {
+                    astronaut.TaskState = AstronautTaskState.IN_TASK;
+                    target.StartTask();
+                }
+            }
+        }
+    }
+}
