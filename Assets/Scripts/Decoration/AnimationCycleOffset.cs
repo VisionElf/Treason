57075d93dfdc65a32a1742ba76d4c2e27228650b
wRadion <wradion@gmail.com>
2020-05-13 18:03:14 +0200

    [WIP]: Only Ambiances+Doors left!

---
+++ Assets/Scripts/Decoration/AnimationCycleOffset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AnimationCycleOffset : MonoBehaviour
+{
+    public float cycleOffset;
+    public string floatName;
+
+    private void Awake()
+    {
+        GetComponent<Animator>().SetFloat(floatName, cycleOffset);
+    }
+}
