ada0b39c61131eb59bf7c2c293c43aa1842e65a5
wRadion <wradion@gmail.com>
2020-05-14 14:22:31 +0200

    [WIP] Finished revamp TheSkeld pixel map

--- Assets/Scripts/Gameplay/Entities/Door.cs
+++ Assets/Scripts/Gameplay/Entities/Door.cs
@@ -5,19 +5,18 @@ namespace Gameplay.Entities
     public class Door : MonoBehaviour
     {
         public float closeDuration;
+        public AudioSource audioSource;
         public AudioClip closeSound;
         public AudioClip openSound;
 
         private Animator _animator;
         private bool _closed;
-        private AudioSource _audioSource;
 
         private static readonly int AnimatorHashClosed = Animator.StringToHash("Closed");
 
         private void Awake()
         {
             _animator = GetComponent<Animator>();
-            _audioSource = GetComponent<AudioSource>();
         }
 
         private void Start()
@@ -29,8 +28,8 @@ namespace Gameplay.Entities
         {
             if (_closed != closed)
             {
-                if (closed) _audioSource.PlayOneShot(closeSound);
-                else _audioSource.PlayOneShot(openSound);
+                if (closed) audioSource.PlayOneShot(closeSound);
+                else audioSource.PlayOneShot(openSound);
             }
             _closed = closed;
             _animator.SetBool(AnimatorHashClosed, _closed);
